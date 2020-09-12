eaca2efe7a11f5cf0ed2340c92636395f527a5df
AdamK222 <basaran@wp.pl>
2020-09-12 20:49:06 +0200

    content type header

--- src/services/HTTP.js
+++ src/services/HTTP.js
@@ -3,6 +3,8 @@ import { toast } from 'react-toastify'
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
+axios.defaults.headers.common['Content-Type'] = "text/plain;charset=utf-8";
+
 axios.interceptors.response.use(null, (error) => {
   const expectedError = error.response && error.response.status >= 400 && error.response.status < 500
 
